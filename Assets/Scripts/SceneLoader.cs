f3dc9ebd533664bb905d874a9e80104c8be29c98
Zulu Inuoe <zulu.inuoe@gmail.com>
2020-02-15 16:33:33 +0000

    Main Menu Mockup

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Simple component to load scenes
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadSceneByIndex(int idx)
+    {
+        SceneManager.LoadScene(idx);
+    }
+}
