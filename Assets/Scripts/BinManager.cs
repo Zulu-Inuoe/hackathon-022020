11cb2c0da1bdf98bf39cbd12899d68363c2cae8c
Ian Valle <ianvalle27@gmail.com>
2020-02-15 18:07:11 +0000

    random magic trash cans

--- Assets/Scripts/BinManager.cs
+++ Assets/Scripts/BinManager.cs
@@ -8,21 +8,44 @@ public class BinManager : MonoBehaviour
     public GameObject plasticBin;
     public GameObject electronicBin;
     public GameObject nonrecyclableBin;
+
+    private List<GameObject> bins = new List<GameObject>();
+    private GameObject tmpObj;
+
+    private int i = 0;
+    private int randIndex;
+    private int maxIndex = 4;
+
     
-    private int index;
-
-    private ArrayList bins = new ArrayList();
-
     
 
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(paperBin.transform());
-        //bins.Add(paperBin, plasticBin, electronicBin, nonrecyclableBin);
+        bins.Add(paperBin);
+        bins.Add(plasticBin);
+        bins.Add(electronicBin);
+        bins.Add(nonrecyclableBin);
+        
+        /*
+        for (int i=0; i<4; i++)
+        {            
+            tmpObj = Instantiate(bins[i], this.GetComponent<Transform>());
+            tmpObj.GetComponent<Transform>().position = new Vector3(((i * 1.5f) - 2.25f), -4.5f, 0);
+        }     
+       */
 
-        //index = (int)Random.range(0, 4);
-       
+       while(bins.Count > 0)
+        {
+            randIndex = Random.Range(0, maxIndex);
+
+            tmpObj = Instantiate(bins[randIndex], this.GetComponent<Transform>());
+            tmpObj.GetComponent<Transform>().position = new Vector3(((i * 1.5f) - 2.25f), -4.5f, 0);
+
+            bins.RemoveAt(randIndex);
+            maxIndex -= 1;
+            i++;
+        }
     }
 
 
