6b7dc444b3052af15264cf826d543bd2fd9fc008
Jose Carlos Hernandez <jose34953@gmail.com>
2020-02-15 18:29:13 +0000

    Spawner And Dragable

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+class DragAndDrop : MonoBehaviour
+{
+
+    public enum ItemType { Plastic, eWaste, Landfill, Paper, none };
+
+    public GameObject Image;
+    public Sprite PlasticSprite;
+    public Sprite EWasteSprite;
+    public Sprite LandfillSprite;
+    public Sprite PaperSprite;
+
+    public Color mouseOverColor = Color.blue;
+    public Color originalColor = Color.yellow;
+    public bool dragging = false;
+    public float distance;
+
+    public ItemType myItem;
+
+    public void OnMouseEnter()
+    {
+       // GetComponent<Renderer>().material.color = mouseOverColor;
+    }
+
+    public void OnMouseExit()
+    {
+       // GetComponent<Renderer>().material.color = originalColor;
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("dds");
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+
+    void Start()
+    {
+        int randomNum = Random.Range(0, 3);
+        
+        switch (randomNum)
+        {
+            case 0:
+                Debug.Log("0");
+                myItem = ItemType.Plastic;
+                this.GetComponent<SpriteRenderer>().sprite = PlasticSprite;
+
+                break;
+
+            case 1:
+                Debug.Log("1");
+                myItem = ItemType.Paper;
+                this.GetComponent<SpriteRenderer>().sprite = EWasteSprite;
+                break;
+
+            case 2:
+                Debug.Log("2");
+                myItem = ItemType.eWaste;
+                 this.GetComponent<SpriteRenderer>().sprite = LandfillSprite;
+
+                break;
+
+            case 3:
+                Debug.Log("3");
+                myItem = ItemType.Landfill;
+                 this.GetComponent<SpriteRenderer>().sprite = PaperSprite;
+                break;
+
+            default:
+                print("Nada");
+                myItem = ItemType.none;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawing : MonoBehaviour
+{
+    public GameObject DragablePrefab;
+
+    GameObject SpawnedObject;
+    
+    void Start()
+    {
+        SpawnItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnItem()
+    {
+        SpawnedObject = Instantiate(DragablePrefab, transform.position, transform.rotation);
+    }
+}
