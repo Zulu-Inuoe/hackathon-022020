28a2676996f5988358b7c87cf93c0e546723bb10
Zulu Inuoe <zulu.inuoe@gmail.com>
2020-02-15 18:51:39 +0000

    Add a loading screen

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Spinner Spinner;
+    public GameObject StartButton;
+    public string NextScene;
+
+    private AsyncOperation m_LoadSceneOp;
+    private void Start()
+    {
+        StartButton.SetActive(false);
+        Spinner.enabled = true;
+        StartCoroutine(LoadSceneCoroutine());
+    }
+
+    private IEnumerator LoadSceneCoroutine()
+    {
+        m_LoadSceneOp = SceneManager.LoadSceneAsync(NextScene);
+        m_LoadSceneOp.allowSceneActivation = false;
+        while (m_LoadSceneOp.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        Spinner.enabled = false;
+        Spinner.transform.rotation = Quaternion.identity;
+        StartButton.SetActive(true);
+    }
+
+    public void ActivateLoadedScene()
+    {
+        m_LoadSceneOp.allowSceneActivation = true;
+    }
+}
---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public float RPM;
+
+    private RectTransform m_Transform;
+
+    private void Start()
+    {
+        m_Transform = GetComponent<RectTransform>();
+    }
+
+    private void FixedUpdate()
+    {
+        var rotation = RPM * (Time.fixedDeltaTime / 60) * 360;
+        m_Transform.Rotate(0, 0, rotation);
+    }
+}
