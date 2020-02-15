6f6da5bc7092e7f6057e5444092532d7bd7c7e62
Ian Valle <ianvalle27@gmail.com>
2020-02-15 16:52:04 +0000

    scene update

---
+++ Assets/Scripts/BinManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinManager : MonoBehaviour
+{
+    public GameObject paperBin;
+    public GameObject plasticBin;
+    public GameObject electronicBin;
+    public GameObject nonrecyclableBin;
+    
+    private int index;
+
+    private ArrayList bins = new ArrayList();
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(paperBin.transform());
+        //bins.Add(paperBin, plasticBin, electronicBin, nonrecyclableBin);
+
+        //index = (int)Random.range(0, 4);
+       
+    }
+
+
+        
+        
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RecyclingBin.cs
+++ Assets/Scripts/RecyclingBin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RecyclingBin : MonoBehaviour
 {
-
+    //
     public int BinType = 0;
     // Start is called before the first frame update
     void Start()
