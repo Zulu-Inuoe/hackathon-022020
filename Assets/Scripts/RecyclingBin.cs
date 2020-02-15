093a982ff78d08d69a823292b5d9e36e1a3f3028
Ian Valle <ianvalle27@gmail.com>
2020-02-15 18:37:28 +0000

    collider on bins

--- Assets/Scripts/RecyclingBin.cs
+++ Assets/Scripts/RecyclingBin.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RecyclingBin : MonoBehaviour
-{
-    //
+{    
     public int BinType = 0;
     // Start is called before the first frame update
     void Start()
